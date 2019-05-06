57ad0d4ddcf49311ed5a3ee351687a0eb0b4a448
Onemeshnik <danillatech@gmail.com>
2019-05-06 22:55:30 +0300

    Update Send script to ffdshow.bat
    FFdshow integration support ffdshow64 too. May be I will add context menu integration later

--- Avisynth/Send script to ffdshow.bat
+++ Avisynth/Send script to ffdshow.bat
@@ -1,1 +1,2 @@
 REG ADD "HKCU\Software\GNU\ffdshow_raw\default" /v "avisynthScriptMULTI_SZ" /t REG_MULTI_SZ /d import(\"%1\")\0#Aye#3a6uB#vapenation /f
+REG ADD "HKCU\Software\GNU\ffdshow64_raw\default" /v "avisynthScriptMULTI_SZ" /t REG_MULTI_SZ /d import(\"%1\")\0#Aye#3a6uB#vapenation /f
